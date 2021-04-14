a959fe58fb72f4e0e618140e7d71966a3b268fa0
Igor Amantino <justin.amantino@gmail.com>
2021-04-14 09:33:23 -0300

    controle do dom

---
+++ script.js
@@ -0,0 +1,6 @@
+//retorna toda as imagens da minha pagina em formato de array
+const img = document.querySelectorAll('img');
+
+//retorna todas as classes da minha pagina em formato de array 
+const SelectClass = document.getElementsByClassName('content')
+console.log(SelectClass)
